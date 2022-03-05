4f86001fc9a3c652b27e4f035d9d47d61af903af
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-03-05 23:58:32 +0100

    Adding index page app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+    @section('content')
+        
+    @include('layouts.nav')
+
+    @include('partials.hero')
+        
+    @include('partials.services')
+
+    @include('partials.offers')
+        
+    @include('partials.testimony')
+
+    @include('partials.blog-section')
+    
+    @include('layouts.footer')
+
+@endsection
