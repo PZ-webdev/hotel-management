799eca8e31b683ef317e60d5c3632f586635d602
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-03-06 12:20:10 +0100

    Adding factories to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(200)->create();
+        \App\Models\RoomType::factory(5)->create();
+        \App\Models\Room::factory(30)->create();
+        \App\Models\Tag::factory(5)->create();
+        \App\Models\HotelTags::factory(500)->create();
+        \App\Models\Rating::factory(2000)->create();
+        \App\Models\Reservation::factory(1000)->create();
     }
 }
