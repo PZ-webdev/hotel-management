5a0f84fbe4e2e948f26825194ff89f8cc6c6cb15
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-03-12 14:31:21 +0100

    Adding sweet alert

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        UxWeb\SweetAlert\SweetAlertServiceProvider::class,
 
     ],
 
@@ -191,7 +192,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'Alert' => UxWeb\SweetAlert\SweetAlert::class,
     ])->toArray(),
 
 ];
