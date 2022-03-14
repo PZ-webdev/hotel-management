e4b4cec7c4f822a298fea0273f339f4aeee275b7
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-03-14 17:41:23 +0100

    adding methods verify rooms

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    public $fillable = ['id_room'];
+
+    public function rating()
+    {
+        return $this->hasMany(Rating::class, 'id_room', 'id');
+    }
+
+
+    private function reservation($room)
+    {
+        return Reservation::select('date_end')
+        ->where('id_room', $room->id)
+        ->whereDate('date_start', '<=', date("Y-m-d"))
+        ->whereDate('date_end', '>=', date("Y-m-d"))
+        ->orderBy('date_end', 'desc')
+        ->first();
+    }
+
+    public function isEmpty($room): bool
+    {
+        $date_end = $this->reservation($room)->date_end;
+        if($date_end > now()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
