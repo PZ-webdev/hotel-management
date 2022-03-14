ea881d34b1d04d23929f5e5ab51f7574a2650012
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-03-14 17:36:03 +0100

    Adding mail reservation

---
+++ resources/views/emails/confirm-reservation.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+    # Potwierdzenie Rezerwacji
+
+    Drogi {{$email->first_name}}  {{$email->last_name}},
+
+    W celu potwierdzenia rezerwacji, kliknij w poniższy przycisk: 
+
+    @component('mail::button', ['url' => route('index'), 'color' => 'success'])
+        Potwierdź
+    @endcomponent
+
+    @component('mail::subcopy')
+        Jeśli to nie Ty dokonałeś rezerwacji prosimy o zignorowanie wiadomości. 
+    @endcomponent
+
+    Dziękujemy,<br>
+    {{ config('app.name') }}
+@endcomponent
