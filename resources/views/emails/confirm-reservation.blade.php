697d3aad5da3615332423b22f259e75681128ff5
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-03-17 09:59:03 +0100

    Updating mail format

--- resources/views/emails/confirm-reservation.blade.php
+++ resources/views/emails/confirm-reservation.blade.php
@@ -1,18 +1,18 @@
 @component('mail::message')
-    # Potwierdzenie Rezerwacji
+# Potwierdzenie Rezerwacji
 
-    Drogi {{$email->first_name}}  {{$email->last_name}},
+Drogi {{$email->first_name}}  {{$email->last_name}},
 
-    W celu potwierdzenia rezerwacji, kliknij w poniższy przycisk: 
+W celu potwierdzenia rezerwacji, kliknij w poniższy przycisk: 
 
-    @component('mail::button', ['url' => route('index'), 'color' => 'success'])
-        Potwierdź
-    @endcomponent
+@component('mail::button', ['url' => route('index'), 'color' => 'success'])
+    Potwierdź
+@endcomponent
 
-    @component('mail::subcopy')
-        Jeśli to nie Ty dokonałeś rezerwacji prosimy o zignorowanie wiadomości. 
-    @endcomponent
+@component('mail::subcopy')
+    Jeśli to nie Ty dokonałeś rezerwacji prosimy o zignorowanie wiadomości, lub skontaktowanie się z administratorem naszej strony. 
+@endcomponent
 
-    Dziękujemy,<br>
-    {{ config('app.name') }}
+Dziękujemy,<br>
+{{ config('app.name') }}
 @endcomponent
