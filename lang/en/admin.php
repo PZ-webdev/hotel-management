c4fb4af820992e90d872c992ac88f9b12ee5f767
Patryk Zaprzała <patryk.zaprzala@gmail.com>
2022-06-07 21:08:13 +0200

    adding translation

---
+++ lang/en/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    
+    'success' => 'Sukces !',
+    'addRommSuccessfully' => 'Poprawnie dodano pokój !',
+    'roomDestroyed' => 'Pokój został usunięty.',
+    'roomIsReserved' => 'Ten pokój jest zarezerwowany !',
+    
+
+]; 
---
+++ lang/en/custom.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    
+    'oops' => 'Uuuups !',
+    'roomNotFound' => 'Wybrany pokój nie istnieje.',
+    'roomIsReservated' => 'Wybrany pokój jest w trakcie rezerwacji do dnia: ',
+    'success' => 'Udało się !',
+    'confirmSendEmail' => 'Potwierdzenie rezerwacji zostało wysłane na podany adres e-mail',
+    'deleteReservation' => 'Rezerwacja została usunięta.',
+    'confirm' => 'Potwierdzone !',
+    'confirmReservation' => 'Twoja rezerwacja została potwierdzona. Do zobaczenia !',
+    'confirmReservationAtDay' => 'Twoja rezerwacja została potwierdzona, dnia: ',
+    'error' => 'Wystąpił błąd',
+    'errorURL' => 'Niepoprawny odnośnik potwierdzający rezerwację.',
+    'information' => 'Informacja',
+
+]; 
